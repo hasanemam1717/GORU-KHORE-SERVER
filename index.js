33c36acea607f9c138a8f42ed2793c7b3d743caa
hasanemam1717 <hasanemam1717@gmail.com>
2024-05-21 05:18:42 +0600

    added delete method

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fglmuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -51,6 +51,13 @@ async function run() {
       const result = await carts.find(query).toArray();
       res.json(result);
     })
+
+    app.delete('/carts/:id' , async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await carts.deleteOne(query);
+      res.send(result);
+    }) 
 
 
 
